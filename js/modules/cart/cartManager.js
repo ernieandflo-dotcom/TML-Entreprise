1cb96735092c71c0ddbc9b8431a05d99b7cf6c68
ernieandflo-dotcom <ernieandflo@protonmail.com>
2025-07-27 11:33:58 -0400

    Update cartManager.js

---
+++ js/modules/cart/cartManager.js
@@ -0,0 +1,14 @@
+export class CartManager {
+  constructor() {
+    this.cart = JSON.parse(localStorage.getItem('cart')) || [];
+  }
+
+  addItem(item) {
+    this.cart.push(item);
+    this._save();
+  }
+
+  _save() {
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+  }
+}
